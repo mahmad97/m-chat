ed22cd02755752df618965937200265b494674d2
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-23 03:34:47 -0400

    add button

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import type { ReactElement, ReactNode } from 'react';
+
+import { buttonTextStyle } from 'components/Typography';
+
+type ButtonProps = Readonly<{
+	id: string;
+	type: 'submit';
+	name: string;
+	children: ReactNode;
+}>;
+
+const Button = (props: ButtonProps): ReactElement => {
+	const backgroundStyle = 'bg-indigo-500 hover:bg-indigo-600';
+
+	return (
+		<button
+			id={props.id}
+			type={props.type}
+			name={props.name}
+			className={`py-2 px-4 ${backgroundStyle} ${buttonTextStyle} rounded-md`}>
+			{props.children}
+		</button>
+	);
+};
+
+export default Button;
