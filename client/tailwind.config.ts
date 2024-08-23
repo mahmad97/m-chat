d1d94862511666a225cc2c1b4030d19064ea9355
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-23 03:28:28 -0400

    add nvmrc file to client + change contexts to hooks in tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
 	content: [
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/context/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/hooks/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	darkMode: 'selector',
 	theme: {
