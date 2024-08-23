b275e4bc294d863e7a4f38fe4724f2026e006546
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-23 03:32:00 -0400

    adjust border color of topbar

--- client/src/components/Topbar/index.tsx
+++ client/src/components/Topbar/index.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from 'components/ThemeToggle';
 
 const Topbar = () => {
 	return (
-		<div className='sticky top-0 w-full border-b backdrop-blur'>
+		<div className='sticky top-0 w-full border-b border-slate-300 dark:border-slate-700 backdrop-blur'>
 			<div className='max-w-screen-xl p-4 mx-auto flex justify-between'>
 				<h1>m-chat</h1>
 				<div className='flex'>
