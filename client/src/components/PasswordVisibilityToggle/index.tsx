88968a5cbd1c3aed02634aa528eacc29465923fd
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-24 01:43:11 -0400

    add visibility toggle component

---
+++ client/src/components/PasswordVisibilityToggle/index.tsx
@@ -0,0 +1,27 @@
+import type { ReactElement } from 'react';
+
+import VisibilityOffSVG from 'img/icons/visibility_off.icon.svg';
+import VisibilityOnSVG from 'img/icons/visibility_on.icon.svg';
+
+type PasswordVisibilityToggleProps = Readonly<{
+	showPassword: boolean;
+	setShowPassword: any;
+}>;
+
+const PasswordVisibilityToggle = (
+	props: PasswordVisibilityToggleProps
+): ReactElement => {
+	return (
+		<div
+			className='pr-2 cursor-pointer'
+			onClick={() => props.setShowPassword(!props.showPassword)}>
+			{props.showPassword ? (
+				<VisibilityOffSVG className='fill-slate-600 dark:fill-slate-400' />
+			) : (
+				<VisibilityOnSVG className='fill-slate-600 dark:fill-slate-400' />
+			)}
+		</div>
+	);
+};
+
+export default PasswordVisibilityToggle;
