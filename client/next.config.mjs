4b750a9a0561342cd2ebfe6d220ea4a4849c586d
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-24 01:39:03 -0400

    add svgr

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: true,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+	experimental: {
+		turbo: {
+			rules: {
+				'*.svg': {
+					loaders: ['@svgr/webpack'],
+					as: '*.js',
+				},
+			},
+		},
+	},
+};
 
 export default nextConfig;
