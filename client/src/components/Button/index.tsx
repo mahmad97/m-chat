89e59f70b060fb9af671b4c5c58d7a92a1fd5504
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-08-27 19:22:27 -0700

    add disabled property to button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -6,17 +6,20 @@ type ButtonProps = Readonly<{
 	id: string;
 	type: 'submit';
 	name: string;
+	disabled?: boolean;
 	children: ReactNode;
 }>;
 
 const Button = (props: ButtonProps): ReactElement => {
-	const backgroundStyle = 'bg-violet-500 hover:bg-violet-600';
+	const backgroundStyle =
+		'bg-violet-500 hover:bg-violet-600 active:bg-violet-700 disabled:bg-violet-300';
 
 	return (
 		<button
 			id={props.id}
 			type={props.type}
 			name={props.name}
+			disabled={props.disabled}
 			className={`py-2 px-4 ${backgroundStyle} ${buttonTextStyle} rounded-md`}>
 			{props.children}
 		</button>
