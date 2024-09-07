32dcde77a4ac9e4911038cd6b5a63a662cd25c29
Mohammad Ahmad <mahmad97taha@gmail.com>
2024-09-07 13:52:37 -0700

    update signup from schema

--- client/src/lib/schemas/signupFormSchema.ts
+++ client/src/lib/schemas/signupFormSchema.ts
@@ -9,7 +9,16 @@ const signupFormSchema = z
 		password: z
 			.string()
 			.min(1, { message: 'Password is required.' })
-			.min(8, { message: 'Password must be atleast 8 characters long.' }),
+			.regex(/^[A-Za-z0-9@!#$._-]*$/, {
+				message: 'Password can only have @!#$._- special characters.',
+			})
+			.min(8, { message: 'Password must be atleast 8 characters long.' })
+			.regex(/.*[a-zA-Z].*/, {
+				message: 'Password must have at least one alphabet.',
+			})
+			.regex(/.*[0-9].*/, {
+				message: 'Password must have at least one number.',
+			}),
 		confirmPassword: z.string().min(1, { message: 'Password is required.' }),
 	})
 	.refine(({ password, confirmPassword }) => password === confirmPassword, {
